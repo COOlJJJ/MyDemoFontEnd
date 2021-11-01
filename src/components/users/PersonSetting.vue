4607600562ecc4be31c6f18a311f320b50d94e1f
xiao chen <2714000135@qq.com>
2021-11-01 16:44:53 +0800

    202111011643

--- src/components/users/PersonSetting.vue
+++ src/components/users/PersonSetting.vue
@@ -3,9 +3,9 @@
     <el-row :gutter="20">
       <el-col :span="6">
         <el-card>
-            <span>个人信息</span>
-           <el-divider></el-divider>
-            <span>基础数据</span>
+          <span>个人信息</span>
+          <el-divider></el-divider>
+          <span>基础数据</span>
         </el-card>
       </el-col>
       <el-col :span="18">
@@ -20,9 +20,24 @@ export default {
   data() {
     return {
 
+
     }
   },
+  created(){
+
+  },
   methods: {
+    //获取单个用户的信息
+    async GetSingleUserInfo() {
+
+      const { data: res } = await this.$http.get("User/GetSingleUserInfo");
+      console.log(res.r)
+
+    },
+    //更改密码
+    ChangePassword() {
+
+    }
 
   }
 }
